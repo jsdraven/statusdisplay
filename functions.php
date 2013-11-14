9ee91b557c3f215d143e04ea1ed692313fea752b
U-FD1LT49\1 <1@FD1LT49.FosterDairyFarms.com>
2013-11-14 08:43:01 -0800

    setup more file structure.

--- functions.php
+++ functions.php
@@ -1,2 +1,20 @@
 <?php
+/**
+ * Filename......: functions.php
+ * Author........: Justin Scott
+ * Created.......: 11/13/13 10:51am
+ * Description...: This is all the functions used within this project.
+ */
 require_once('config.php');
+
+function DbConnection($query){
+
+    $DB_User = constant('dbUser');
+    $DB_Password = constant('dbPass');
+    $DB_Host = constant('dbHost');
+    $DB_Name = constant('dbname');
+    $copperrun = mysqli_connect($DB_Host, $DB_User, $DB_Password, $DB_Name);
+    $result = mysqli_query($copperrun, $query);
+    mysqli_close($copperrun);
+    return $result;
+}
